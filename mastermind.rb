5b34bd548c1c6a50c85a3bb4b829f4efff814c58
Calvin Conley <cconley0125@gmail.com>
2020-05-20 17:34:09 -0400

    mastermind working for guesses without duplicates

---
+++ mastermind.rb
@@ -0,0 +1,61 @@
+require 'colorize'
+
+#constants for colored circles
+RED = "\u2B24".red
+GREEN = "\u2B24".green
+BLUE = "\u2B24".cyan
+YELLOW = "\u2B24".yellow
+PINK = "\u2B24".magenta
+WHITE = "\u2B24".white
+SMALL_RED = "\u25CF".red
+SMALL_WHITE = "\u25CF".white
+
+# create class for computer and add function for randomly selecting colors
+class Computer
+    def self.choose_code
+        colors = [RED, GREEN, BLUE, YELLOW, PINK, WHITE]
+        colors.sample(4)
+    end
+end
+
+#create player class and add function for guessing
+class User
+    def self.guess
+        user_guess = gets.chomp.downcase
+        user_guess = user_guess.gsub("r", RED)
+        user_guess = user_guess.gsub("g", GREEN)
+        user_guess = user_guess.gsub("b", BLUE)
+        user_guess = user_guess.gsub("y", YELLOW)
+        user_guess = user_guess.gsub("p", PINK)
+        user_guess = user_guess.gsub("w", WHITE)
+        return user_guess.split(" ")
+    end
+end
+
+# function for checking guess
+def check_guess(guess, code)
+    feedback = []
+    guess.each_with_index do |color, index|
+        if color == code[index]
+            feedback.unshift(SMALL_RED)
+        elsif code.include?(color)
+            feedback.push(SMALL_WHITE)
+        end
+    end
+    return feedback
+end
+
+
+#12 times loop for 12 guesses during game
+puts "Choose 4 colors to guess. \nType the colors separated by spaces. \nChoices are red, green, blue, yellow, pink, and white. \nEnter the first letter of each color to select."
+code = Computer.choose_code
+puts code.join("  ")
+12.times do
+    user_guess = User.guess
+    puts user_guess.join("  ") + "   " + check_guess(user_guess, code).join("  ")
+    if user_guess == code
+        puts "You Win!!"
+        puts code.join("  ")
+        break
+    end
+end
