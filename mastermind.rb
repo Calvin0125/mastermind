7ad13e3cd1917d7ea69ad06833b9ff78ef0c3966
Calvin Conley <cconley0125@gmail.com>
2020-05-21 15:03:53 -0400

    player can either guess code or make code for computer to guess

--- mastermind.rb
+++ mastermind.rb
@@ -10,11 +10,19 @@ WHITE = "\u2B24".white
 SMALL_RED = "\u25CF".red
 SMALL_WHITE = "\u25CF".white
 
-# create class for computer and add function for randomly selecting colors
+# create class for computer and add function for randomly selecting colors and computer guess
 class Computer
-    def self.choose_code
+    def self.choose_hard_code
         colors = [RED, GREEN, BLUE, YELLOW, PINK, WHITE]
         Array.new(4) {colors.sample}
+    end
+    def self.choose_easy_code
+        colors = [RED, GREEN, BLUE, YELLOW, PINK, WHITE]
+        colors.sample(4)
+    end
+    def self.guess
+        colors = [RED, GREEN, BLUE, YELLOW, PINK, WHITE]
+        colors.sample(4)
     end
 end
 
@@ -22,6 +30,10 @@ end
 class User
     def self.guess
         user_guess = gets.chomp.downcase
+        while user_guess !~ /\A[rgbypw]\s[rgbypw]\s[rgbypw]\s[rgbypw]\z/ do
+            puts "Enter the first letter of each color separated by spaces."
+            user_guess = gets.chomp.downcase
+        end
         user_guess = user_guess.gsub("r", RED)
         user_guess = user_guess.gsub("g", GREEN)
         user_guess = user_guess.gsub("b", BLUE)
@@ -30,35 +42,110 @@ class User
         user_guess = user_guess.gsub("w", WHITE)
         return user_guess.split(" ")
     end
+    def self.choose_code
+        puts "Choose your code for the computer to guess. \nEnter the first letter of each color separated by spaces."
+        code = gets.chomp.downcase
+        while code !~ /\A[rgbypw]\s[rgbypw]\s[rgbypw]\s[rgbypw]\z/ do
+            puts "Enter the first letter of each color separated by spaces."
+            code = gets.chomp.downcase
+        end
+        code = code.gsub("r", RED)
+        code = code.gsub("g", GREEN)
+        code = code.gsub("b", BLUE)
+        code = code.gsub("y", YELLOW)
+        code = code.gsub("p", PINK)
+        code = code.gsub("w", WHITE)
+        return code.split(" ")
+    end
 end
 
 # function for checking guess
+# global variable to use in Computer class for improving Computer guesses
+$feedback = []
 def check_guess(guess, code)
-    feedback = []
+    $feedback = []
     already_checked = []
     guess.each_with_index do |color, index|
         if color == code[index]
-            feedback.unshift(SMALL_RED)
+            $feedback.unshift(SMALL_RED)
             already_checked.push(color)
+            if already_checked.count(color) > code.count(color)
+                $feedback.delete_at($feedback.index(SMALL_WHITE))
+            end
         elsif code.include?(color) && already_checked.count(color) < code.count(color)
-            feedback.push(SMALL_WHITE)
+            $feedback.push(SMALL_WHITE)
             already_checked.push(color)
         end
     end
-    return feedback
+    return $feedback
 end
 
 
-#12 times loop for 12 guesses during game
-puts "Choose 4 colors to guess. \nType the colors separated by spaces. \nChoices are red, green, blue, yellow, pink, and white. \nEnter the first letter of each color to select."
-code = Computer.choose_code
-puts code.join("  ")
-12.times do
-    user_guess = User.guess
-    puts user_guess.join("  ") + "   " + check_guess(user_guess, code).join("  ")
-    if user_guess == code
-        puts "You Win!!"
-        puts code.join("  ")
-        break
+#choose difficulty
+def choose_difficulty    
+    puts "Choose hard (duplicate colors allowed) or easy (no duplicate colors). \nType 'h' or 'e' to choose."
+    difficulty = gets.chomp.downcase
+    while difficulty !~ /\A[he]\z/ do
+        puts "Type 'h' or 'e' to choose difficulty."
+        difficulty = gets.chomp.downcase
+    end
+    puts "Choose 4 colors to guess. \nType the colors separated by spaces. \nChoices are red, green, blue, yellow, pink, and white. \nEnter the first letter of each color to select."
+    if difficulty == "h"
+        code = Computer.choose_hard_code
+    elsif difficulty == "e"
+        code = Computer.choose_easy_code
+    end
+    return code
+end
+
+#guess computers code
+def player_guess(code)   
+    accumulator = 0
+    12.times do
+        user_guess = User.guess
+        puts user_guess.join("  ") + "   " + check_guess(user_guess, code).join("  ")
+        if user_guess == code
+            puts "You Win!!"
+            puts code.join("  ")
+            break
+        end
+        accumulator += 1
+        if accumulator == 12
+            puts "You Lost"
+        end
     end
 end
+
+#computer guesses players code
+def computer_guess(code)
+    accumulator = 0
+    12.times do
+        computer_guess = Computer.guess
+        puts computer_guess.join("  ") + "   " + check_guess(computer_guess, code).join("  ")
+        if computer_guess == code
+            puts "The computer guessed your code. You lose."
+            break
+        end
+        accumulator += 1
+        if accumulator == 12
+            puts "The computer couldn't guess your code. You win!"
+        end
+    end
+end
+#choose if making code or guessing code
+puts "Type 'm' to make a code and 'g' to guess a code."
+choice = gets.chomp.downcase
+while choice !~ /\A[mg]\z/ do
+    puts "Type 'm' to make a code and 'g' to guess a code."
+    choice = gets.chomp.downcase
+end
+if choice == 'g'
+    computer_code = choose_difficulty
+    puts computer_code
+    player_guess(computer_code)
+elsif choice == 'm'
+    player_code = User.choose_code
+    puts "Your code is #{player_code.join("  ")}"
+    computer_guess(player_code)
+end
+
